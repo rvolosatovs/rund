34e503ed668e95fe44d87de11aa8103b31865531
Roman Volosatovs <rvolosatovs@riseup.net>
2021-04-10 00:11:29 +0200

    shell.nix: add scripts

--- shell.nix
+++ shell.nix
@@ -4,5 +4,19 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     go
     openssl
+
+    (
+      pkgs.writeShellScriptBin "serve" ''
+        set -e
+        ${pkgs.fd}/bin/fd --glob '*.go' cmd/rund cmd/internal pkg/job pkg/rund | ${pkgs.entr}/bin/entr -rs '${pkgs.go}/bin/go build -o ./bin/rund ./cmd/rund && sudo ./bin/rund serve ''${@}'
+    ''
+    )
+
+    (
+      pkgs.writeShellScriptBin "cli" ''
+        set -e
+        ${pkgs.go}/bin/go run ./cmd/runc ''${@}
+    ''
+    )
   ];
 }
